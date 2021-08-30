b80836198bda68aab7feb5e1beb7cbaa1b12ffd6
Pedro Gaya <pedrogaya@gmail.com>
2021-08-30 00:56:44 -0300

    reversing

--- src/components/TabelaPedidos/index.js
+++ src/components/TabelaPedidos/index.js
@@ -35,7 +35,7 @@ function TabelaPedidos({ setModalDetalhe, setIdPedido, toggle }) {
         });
 
         const dados = await response.json();
-        if (!response.ok && response.status !== 404) {
+        if (!response.ok) {
           setLoading(false);
           toast.error('Erro ao buscar pedidos.', toastOpts);
           return;
